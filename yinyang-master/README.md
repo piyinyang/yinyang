9e83763f8a2ce5a4e9513cd22e477df4c9b931cc
jaohuller <joaohuller@gmail.com>
2019-10-31 17:23:40 -0300

    Add files via upload
    Modificação dos sprites de animação. Modificação no combate quando o slime ataca o player.Modificação do spritesheet de corrida e adição do spritesheet geral, mas sem usa-lo.

---
+++ yinyang-master/README.md
@@ -0,0 +1,7 @@
+# Yin Yang
+Esse é nosso Projeto Integrador da oitava fase do curso de telecomunicações do IFSC - São José.
+Os integrantes do grupo são:
+- Andrey
+- João Vinicius
+- Eduardo
+- Grasiele
---
+++ yinyang-master/index.js
@@ -0,0 +1,26 @@
+import { GameScene } from "./GameScene.js";
+import { start } from "./start.js";
+import { gameover } from "./gameover.js";
+
+var config = {
+  type: Phaser.AUTO,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 800 },
+      debug: true
+    }
+  },
+  // Suporte a tela cheia
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: "game",
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 600
+  },
+  // VÃÂ¡rias cenas, em sequÃÂªncia
+  scene: [start, GameScene, gameover]
+};
+
+var game = new Phaser.Game(config);
